6ed27eb1820c650d42b0b2eb24131d5263557294
D3lta-M3 <69670896+D3lta-M3@users.noreply.github.com>
2021-02-20 16:03:53 +0800

    Learned more about qLayout
    Started trying to create ok and cancel buttons

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import sys
 
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QHBoxLayout, QPushButton, QVBoxLayout, QGridLayout
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QHBoxLayout, QPushButton, QVBoxLayout, QGridLayout, \
+    QFormLayout, QLineEdit, QDialogButtonBox, QDialog
 
 # Hello World in Application
 app = QApplication(sys.argv)
 window = QWidget()
 window.setWindowTitle('PyQt5 App')
-window.setGeometry(100, 100, 1280, 720)
+window.setGeometry(0, 0, 800, 40)
+# ( where it starts X, where it starts Y, horizontal, vertical)
 window.move(60, 15)
-helloMsg = QLabel('<h1>Hello World!</h1>', parent=window)
-helloMsg.move(60, 15)
+# helloMsg = QLabel('<h1>Hello World!</h1>', parent=window)
+# helloMsg.move(60, 15)
 window.show()
 
 # Should create three different boxes horizontally
@@ -30,16 +32,30 @@ window.show()
 
 # Note that QV and QH can't be in the same area as they are right now, because the first one overrides the second.
 
-layout = QGridLayout()
-layout.addWidget(QPushButton('Button (0, 0)'), 0, 0)
-layout.addWidget(QPushButton('Button (0, 1)'), 0, 1)
-layout.addWidget(QPushButton('Button (0, 2)'), 0, 2)
-layout.addWidget(QPushButton('Button (1, 0)'), 1, 0)
-layout.addWidget(QPushButton('Button (1, 1)'), 1, 1)
-layout.addWidget(QPushButton('Button (1, 2)'), 1, 2)
-layout.addWidget(QPushButton('Button (2, 0)'), 2, 0)
-layout.addWidget(QPushButton('Button (2, 1) + 2 Columns Span'), 2, 1, 1, 2)
+
+# Grid Layout Widget
+# layout = QGridLayout()
+# layout.addWidget(QPushButton('Button (0, 0)'), 0, 0)
+# layout.addWidget(QPushButton('Button (0, 1)'), 0, 1)
+# layout.addWidget(QPushButton('Button (0, 2)'), 0, 2)
+# layout.addWidget(QPushButton('Button (1, 0)'), 1, 0)
+# layout.addWidget(QPushButton('Button (1, 1)'), 1, 1)
+# layout.addWidget(QPushButton('Button (1, 2)'), 1, 2)
+# layout.addWidget(QPushButton('Button (2, 0)'), 2, 0)
+# layout.addWidget(QPushButton('Button (2, 1) + 2 Columns Span'), 2, 1, 1, 2)
+# window.setLayout(layout)
+
+# Form Layout
+layout = QFormLayout()
+layout.addRow('Name:', QLineEdit())
 window.setLayout(layout)
+# layout.addRow('Age:', QLineEdit())
+# layout.addRow('Job:', QLineEdit())
+# layout.addRow('Hobbies:', QLineEdit())
+layout = QDialogButtonBox()
+btns = QDialogButtonBox()
+btns.setStandardButtons(
+    QDialogButtonBox.Cancel | QDialogButtonBox.Ok)
 
 
 sys.exit(app.exec_())
