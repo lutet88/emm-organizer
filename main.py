122b7bbe1c0c35ca0cad56b8530d3d1fc223f230
D3lta-M3 <69670896+D3lta-M3@users.noreply.github.com>
2021-02-20 15:38:10 +0800

    Learning how to use QWidgests and testing their compatibility with each other

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import sys
 
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QHBoxLayout, QPushButton, QVBoxLayout, QGridLayout
 
+# Hello World in Application
 app = QApplication(sys.argv)
 window = QWidget()
 window.setWindowTitle('PyQt5 App')
@@ -9,7 +10,36 @@ window.setGeometry(100, 100, 1280, 720)
 window.move(60, 15)
 helloMsg = QLabel('<h1>Hello World!</h1>', parent=window)
 helloMsg.move(60, 15)
-
 window.show()
+
+# Should create three different boxes horizontally
+# layout = QHBoxLayout()
+# layout.addWidget(QPushButton('Left'))
+# layout.addWidget(QPushButton('Center'))
+# layout.addWidget(QPushButton('Right'))
+# window.setLayout(layout)
+# window.show()
+
+# Should create three different boxes vertically
+# layout = QVBoxLayout()
+# layout.addWidget(QPushButton('Top'))
+# layout.addWidget(QPushButton('Center'))
+# layout.addWidget(QPushButton('Bottom'))
+# window.setLayout(layout)
+# window.show()
+
+# Note that QV and QH can't be in the same area as they are right now, because the first one overrides the second.
+
+layout = QGridLayout()
+layout.addWidget(QPushButton('Button (0, 0)'), 0, 0)
+layout.addWidget(QPushButton('Button (0, 1)'), 0, 1)
+layout.addWidget(QPushButton('Button (0, 2)'), 0, 2)
+layout.addWidget(QPushButton('Button (1, 0)'), 1, 0)
+layout.addWidget(QPushButton('Button (1, 1)'), 1, 1)
+layout.addWidget(QPushButton('Button (1, 2)'), 1, 2)
+layout.addWidget(QPushButton('Button (2, 0)'), 2, 0)
+layout.addWidget(QPushButton('Button (2, 1) + 2 Columns Span'), 2, 1, 1, 2)
+window.setLayout(layout)
+
 
 sys.exit(app.exec_())
