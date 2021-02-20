47762bb935471572d6e50d8e76abef48168a1c29
D3lta-M3 <69670896+D3lta-M3@users.noreply.github.com>
2021-02-20 18:51:16 +0800

    Started Creation of Array for Touchpad Display

---
+++ definition testing.py
@@ -0,0 +1,18 @@
+import sys
+
+from PyQt5.QtWidgets import QApplication, QGridLayout, QPushButton, QWidget
+
+app = QApplication(sys.argv)
+window = QWidget()
+window.showFullScreen()
+
+
+layout = QGridLayout()
+layout.addWidget(QPushButton('Button (0, 0)'), 0, 0)
+layout.addWidget(QPushButton('Button (0, 0)'), 0, 1)
+layout.addWidget(QPushButton('Button (0, 0)'), 0, 2)
+layout.addWidget(QPushButton('Button (0, 0)'), 0, 3)
+layout.addWidget(QPushButton('Button (0, 0)'), 0, 4)
+window.setLayout(layout),
+
+sys.exit(app.exec_())
