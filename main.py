f4aa86a923081460475190265d0aaf126480d972
Antarctic Petrel <78806646+Antarctic-Petrel@users.noreply.github.com>
2021-02-21 10:00:59 +0800

    Added Documentation

--- main.py
+++ main.py
@@ -7,26 +7,28 @@ from hwcode.RGBController import RGBController
 from hwcode.pinmaps import get_pinmapper
 from database import Database
 
-# Hello World in Application
+# Start App
 app = QApplication(sys.argv)
 window = QWidget()
 
+
+# If on Linux use /deb/ttyACM0, if not (assuming on window) use COM14
 os = platform.system()
 
 if os == "Linux":
     comport = "/dev/ttyACM0"
 else:
     comport = "COM14"
+    
 
-
-
-
-dist = platform.platform()
+# RGB strips initilize
 mapper = get_pinmapper()
 rgb = RGBController(comport)
 rgb.clear()
 db = Database("db5.json", testing=True)
 
+# If on an AMR version of Debian run full screen, if not then run in windowed mode at a fixed resolution
+dist = platform.platform()
 
 if dist == "Linux-5.10.11-v7l+-armv7l-with-debian-10.8":
     window.showFullScreen()
@@ -36,7 +38,7 @@ else:
     window.show()
 
 
-
+# Stylesheets
 buttonstyle = """
     QWidget {
             font-family: "DejaVu Sans Mono";
@@ -76,19 +78,24 @@ cabinethighlightedstyle = """
             color: rgb(56, 56, 56);
         """
 
+# Usless Function
 def DisplayText():
     QLabel("Pog")
 
+# Layout Managment
+
+# Create Layouts
 allLayout = QVBoxLayout()
 mainLayout = QGridLayout()
 editorLayout = QGridLayout()
 mainWidget = QWidget()
 editorWidget = QWidget()
 
-
+# Set widets to layouts
 mainWidget.setLayout(mainLayout)
 editorWidget.setLayout(editorLayout)
 
+# Add these layout widgets to the main layout
 allLayout.addWidget(mainWidget)
 allLayout.addWidget(editorWidget)
 
@@ -98,26 +105,31 @@ layer = 0
 
 
 # Main Menu
+
+#Menu Title
 MenuTitle = QLabel("Smart Cabinet")
 MenuTitle.setStyleSheet(titlestyle)
 MenuTitle.setAlignment(Qt.AlignCenter)
 
+#Create Buttons
 class ButtonLayout:
     def __init__(self, names):
         self.buttons = [QPushButton() for x in range(4)]
         for buttonIndex in range(4):
-            self.buttons[buttonIndex].setStyleSheet(buttonstyle)
-            self.buttons[buttonIndex].setText(names[buttonIndex])
-    def changeText(self, id, text):
+            self.buttons[buttonIndex].setStyleSheet(buttonstyle) #Set Style
+            self.buttons[buttonIndex].setText(names[buttonIndex]) #Set Names
+    def changeText(self, id, text): #Change text for one button
         self.buttons[id].setText(text)
-    def changeTexts(self, texts):
+    def changeTexts(self, texts): #Change text for all buttons
         for buttonIndex in range(4):
             self.buttons[buttonIndex].setText(texts[buttonIndex])
-    def connectButton(self, id, connection):
+    def connectButton(self, id, connection): #Connect buttons to events
         self.buttons[id].clicked.connect(connection)
 
-buttons = ButtonLayout(["Quick Access", "Manage Drawers", "My Projects", "Check Supply"])
+buttons = ButtonLayout(["Quick Access", "Manage Drawers", "My Projects", "Check Supply"]) #Button Names
 
+
+#Button Functions
 def Button1pressed():
     buttonPressed(0)
 
@@ -130,12 +142,14 @@ def Button3pressed():
 def Button4pressed():
     buttonPressed(3)
 
+#Connection to functions
 buttons.connectButton(0, Button1pressed)
 buttons.connectButton(1, Button2pressed)
 buttons.connectButton(2, Button3pressed)
 buttons.connectButton(3, Button4pressed)
 
-mainLayout.addWidget(MenuTitle, 0, 0, 1, 2)
+#Add Widgets to layout
+mainLayout.addWidget(MenuTitle, 0, 0, 1, 2) 
 mainLayout.addWidget(buttons.buttons[0], 1, 0)
 mainLayout.addWidget(buttons.buttons[1], 1, 1)
 mainLayout.addWidget(buttons.buttons[2], 2, 0)
@@ -144,26 +158,25 @@ mainLayout.addWidget(buttons.buttons[3], 2, 1)
 def buttonPressed(button):
     global layer
     print("button", button, "pressed")
-    if layer == 0 and button == 3:
-        # manage drawers
+    if layer == 0 and button == 3: # Set layer to manage drawers
         layer = 1
         mainWidget.hide()
         editorWidget.show()
         for button in range(20):
             (r, g, b) = (int(x * 0.14) for x in db.getColor(button))
-            rgb.fillStrip(mapper.getMapBy2DIndex(button).value, r, g, b, True)
+            rgb.fillStrip(mapper.getMapBy2DIndex(button).value, r, g, b, True) # load colors
         
     elif layer == 0 and button == 2:
         # etc etc
         pass
-    elif layer == 1 and button == 0:
+    elif layer == 1 and button == 0: #Reurn to main menu
         layer = 0
         buttons.changeTexts(["Quick Access", "Manage Drawers", "My Projects", "Check Supply"])
         
-names = ["John", "William", "Charles", "James", "George", "Frank", "Joseph", "Henry", "Thomas", "Harry"]
+names = ["John", "William", "Charles", "James", "George", "Frank", "Joseph", "Henry", "Thomas", "Harry"] # Generic name list
 
 
-def getColoredStyle(x, y):
+def getColoredStyle(x, y): # Calculate color based on position and stock
     quant = db.getQuantity(4 * x + y)
     maximum = db.getMaximum(4 * x + y)
     diff = quant / maximum
@@ -182,10 +195,10 @@ def handleCabinetButtons(i):
 
     print("entry: prevcoord:", prevcoord, prevrealcoord)
     (x, y) = i
-    print("button", x, y, "pressed")
+    print("button", x, y, "pressed")  #Print log messages
     global rgb, mapper, layer
     
-    # fill rgb for all cells
+    # set rgb for all cells
     if prevcoord:
         if prevcoord[0] == x and prevcoord[1] == y:
             for button in range(20):
@@ -224,18 +237,18 @@ class CabinetLayout:
         self.buttons = [[QPushButton() for x in range(4)] for y in range(5)]
         for x in range(5):
             for y in range(4):
-                (r, g, b) = (str(k) for k in db.getColor(4 * x + y))
-                self.buttons[x][y].setStyleSheet(cabinetstyle+"background-color: rgb("+r+", "+g+", "+b+")}")
-                self.buttons[x][y].setText(db.getName(4 * x + y))
+                (r, g, b) = (str(k) for k in db.getColor(4 * x + y))#Get color
+                self.buttons[x][y].setStyleSheet(cabinetstyle+"background-color: rgb("+r+", "+g+", "+b+")}") #Set background color
+                self.buttons[x][y].setText(db.getName(4 * x + y)) #Set Names from DB
     def assignToLayout(self, layout):
         for x in range(5):
             for y in range(4):
-                layout.addWidget(self.buttons[x][y], y, x)
+                layout.addWidget(self.buttons[x][y], y, x) #Add buttons to layout
     def connectButtons(self):
         for x in range(5):
             for y in range(4):
-                print("connecting ", self.buttons[x][y], "to", x, y)
-                self.buttons[x][y].clicked.connect(lambda state, i=(x, y): handleCabinetButtons(i))
+                print("connecting ", self.buttons[x][y], "to", x, y) #loging
+                self.buttons[x][y].clicked.connect(lambda state, i=(x, y): handleCabinetButtons(i)) #Connect Buttons to the handler
 
 
 
@@ -248,6 +261,7 @@ class CabinetLayout:
 EditorTitle = QLabel("Brexit")
 EditorTitle.setStyleSheet(titlestyle)
 
+#assign cabinet layout to editor layout
 cabinet = CabinetLayout()
 cabinet.assignToLayout(editorLayout)
 cabinet.connectButtons()
